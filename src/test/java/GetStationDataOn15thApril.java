37e0c5f3a5d6b0e27497c9e8eb85d7b8d6359ce0
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2021-11-17 09:12:04 +0530

    added tests for validating response body, content type and logging requesta nd response details

--- src/test/java/GetStationDataOn15thApril.java
+++ src/test/java/GetStationDataOn15thApril.java
@@ -1,4 +1,6 @@
+import io.restassured.http.ContentType;
 import org.junit.Test;
+import static org.hamcrest.Matchers.*;
 
 import static io.restassured.RestAssured.given;
 
@@ -12,5 +14,39 @@ public class GetStationDataOn15thApril {
         then().
             assertThat().
                 statusCode(200);
+    }
+
+    @Test
+    public void testGetStationDataOn15thAprilAndValidateForContentType(){
+        given().
+        when().
+             get("https://api.bart.gov/api/route.aspx?cmd=routes&key=MW9S-E7SL-26DU-VV8V&date=04/15/2021&json=y").
+        then().
+             assertThat().
+//                contentType("application/json");
+                  contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void testGetStationDataOn15thAprilAndLogRequestAndResponseDetails(){
+        given().
+             log().all().
+        when().
+             get("https://api.bart.gov/api/route.aspx?cmd=routes&key=MW9S-E7SL-26DU-VV8V&date=04/15/2021&json=y").
+        then().
+             log().body();
+    }
+
+    @Test
+    public void testGetStationDataOn15thAprilAndValidateResponseBody(){
+        given().
+        when().
+              get("https://api.bart.gov/api/route.aspx?cmd=routes&key=MW9S-E7SL-26DU-VV8V&date=04/15/2021&json=y").
+        then().
+              assertThat().
+                body("root.routes.route[0].name", equalTo("Oakland Airport to Coliseum")).
+                body("root.routes.route.routeID", hasItem("ROUTE 20")).
+                body("root.routes.route.routeID", not(hasItem("ROUTE 0"))).
+                body("root.routes.route.number", hasSize(12));
     }
 }
