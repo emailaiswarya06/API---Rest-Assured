480c0ccc3b40be3efe68d82922b5d4c8ca4383a9
emailaiswarya06 <emailaiswarya06@gmail.com>
2021-11-27 23:52:07 +0530

    updated CreateUserForPetStore

--- src/test/java/PetStore/API/CreateUserForPetStore.java
+++ src/test/java/PetStore/API/CreateUserForPetStore.java
@@ -1,17 +1,12 @@
 package PetStore.API;
 
-import PetStore.Model.UserArrayCreateForPetStoreRequestModel;
 import PetStore.Model.UserCreateForPetRequestModel;
 import PetStore.Model.UserCreateForPetResponseModel;
-import Serialization.UserCreateRequestModel;
-import Serialization.UserCreateResponseModel;
 import io.restassured.http.ContentType;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
 
@@ -46,10 +41,12 @@ public class CreateUserForPetStore {
                 assertThat().statusCode(200);
 
     }
-// serialization - array of pet store users and deserialization to extract the response and print
+    // serialization - array of pet store users and deserialization to extract the response and print
     @Test
     public void shouldCreateArrayOfUsersForPetStore(){
-        List<UserArrayCreateForPetStoreRequestModel> userAll=new ArrayList<>();
+        /*
+      List<UserArrayCreateForPetStoreRequestModel> userAll=new ArrayList<>();
+
         //User1
         UserArrayCreateForPetStoreRequestModel userArrayRequest1 = new UserArrayCreateForPetStoreRequestModel();
         userArrayRequest1.setUserArray(Arrays.asList(111,"PeterForPets","Peter","Parker","peter.parker@gmail.com","PeterForPets","9016527676",1));
@@ -57,16 +54,33 @@ public class CreateUserForPetStore {
         //User 2
         UserArrayCreateForPetStoreRequestModel userArrayRequest2 = new UserArrayCreateForPetStoreRequestModel();
         userArrayRequest2.setUserArray(Arrays.asList(222,"AmyLovesCats","Amy","Williams","amy.williams@gmail.com","AmyLovesCats","9016011122",0));
+*/
+        List<UserCreateForPetRequestModel> userArray = new ArrayList<>();
+        UserCreateForPetRequestModel user1 = new UserCreateForPetRequestModel();
+        user1.setId(111);
+        user1.setUsername("PeterForPets");
+        user1.setFirstName("Peter");user1.setLastName("Parker");
+        user1.setEmail("peter.parker@gmail.com");user1.setPassword("PeterForPets");
+        user1.setPhone("9016527676");user1.setUserStatus(1);
+        userArray.add(user1);
+
+
+        UserCreateForPetRequestModel user2 = new UserCreateForPetRequestModel();
+        user2.setId(222);
+        user2.setUsername("AmyLovesCats");
+        user2.setFirstName("Amy");user2.setLastName("Williams");
+        user2.setEmail("amy.williams@gmail.com");user2.setPassword("AmyLovesCats");
+        user2.setPhone("9016011122");user2.setUserStatus(0);
+
+        userArray.add(user2);
 
         UserCreateForPetResponseModel userAllResponseBody =
-        given().
-                contentType(ContentType.JSON).body(userAll).
-                when().
-                post("https://petstore.swagger.io/v2/user/createWithArray").as(UserCreateForPetResponseModel.class);
-//                then().
-//                assertThat().statusCode(200);
+                given().
+                        contentType(ContentType.JSON).body(userArray).
+                        when().
+                        post("https://petstore.swagger.io/v2/user/createWithArray").as(UserCreateForPetResponseModel.class);
 
-        System.out.println(userAllResponseBody);
+        System.out.println(userAllResponseBody.toString());
 
         Assert.assertEquals(200,userAllResponseBody.getCode());
     }
